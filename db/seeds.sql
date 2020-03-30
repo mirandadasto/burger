1e1797858813e80da721bea49f3e0f74a518c406
Miranda D'Asto <merlinockipella@gmail.com>
2020-03-30 14:26:01 -0600

    Created files as specified in homework instructions.

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+    INSERT INTO burgers (burger_name, devoured)
+        VALUES ('Bacon Cheese Burgers', 0),
+                ('Jr Hamburger', 0),
+                ('Jr Cheeseburger', 0),
+                ('Mushroom Swiss Burger', 0);
